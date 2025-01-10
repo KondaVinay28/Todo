f56f1d3d59deccb7d69bee2c3756b8a554da2af8
KondaVinay28 <kondavinay555@gmail.com>
2025-01-10 08:49:27 +0530

    Todo

---
+++ index.js
@@ -0,0 +1,22 @@
+const inp = document.querySelector("input");
+const btn = document.querySelector("button");
+const ul = document.querySelector("ul");
+const list = document.querySelector("li");
+const delBtn = document.querySelectorAll("delBtn");
+//Displaying list-item
+btn.addEventListener("click", function () {
+  let val = inp.value;
+  const el = document.createElement("li");
+  el.innerText = val;
+  ul.appendChild(el);
+  inp.value = "";
+  // creating delete button
+  const renBtn = document.createElement("button");
+  const deleteButton = renBtn.classList.add("delBtn");
+  renBtn.innerText = "Delete";
+  el.appendChild(renBtn);
+});
+//functionality of delete button
+deleteButton.addEventListener("click", function () {
+  ul.parentNode.remove();
+});
